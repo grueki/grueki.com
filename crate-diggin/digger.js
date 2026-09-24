1f67b96ccfae235ae02919928b98a33e8c272ed1
Izzy Johns <isaac.y.johns@gmail.com>
2026-09-25 01:23:53 +1000

    inital anim in a good place

--- crate-diggin/digger.js
+++ crate-diggin/digger.js
@@ -2,9 +2,18 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 async function dig() {
+    const button = document.getElementById("all-records");
+    button.classList.add("all-records");
+
+    await wait(2000);
+
     var json = await fetch('./records.json');
     var albums = await json.json();
     var num = getRandomInt(albums.length);
     document.getElementById("album-name").textContent = albums[num].title;
+
+    
 }
