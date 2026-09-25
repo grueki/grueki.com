7142594ca356f643d397314f38fcb3bfaf93429c
Izzy Johns <isaac.y.johns@gmail.com>
2026-09-25 14:52:20 +1000

    album images and functionality done

--- crate-diggin/digger.js
+++ crate-diggin/digger.js
@@ -4,16 +4,54 @@ function getRandomInt(max) {
 
 const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+window.addEventListener('load', async () => {
+  var json = await fetch('./records.json');
+  var albums = await json.json();
+
+  const records = [...document.getElementsByClassName("record-sleeve")];
+
+  records.forEach(element => {
+    element.style.backgroundImage = "url(./record-img/" + albums[getRandomInt(albums.length)].image + ")";
+  });
+});
+
 async function dig() {
-    const button = document.getElementById("all-records");
-    button.classList.add("all-records");
+  var json = await fetch('./records.json');
+  var albums = await json.json();
+  var albumToDisplay = getRandomInt(albums.length);
 
-    await wait(2000);
+  const digButton = document.getElementById("dig-button");
+  digButton.disabled = true;
 
-    var json = await fetch('./records.json');
-    var albums = await json.json();
-    var num = getRandomInt(albums.length);
-    document.getElementById("album-name").textContent = albums[num].title;
+  const allRecords = document.getElementById("all-records");
+  allRecords.classList.add("all-records");
 
-    
+  const finals = [...document.getElementsByClassName("final")];
+  finals.forEach(element => {
+    element.style.backgroundImage = "url(./record-img/" + albums[albumToDisplay].image + ")";
+  });
+
+  const albumInfo = document.getElementById("album-info");
+  albumInfo.innerHTML = "";
+
+  const realFinal = document.getElementById("final-record");
+  realFinal.style.display = "none";
+  allRecords.style.display = "block";
+
+  addEventListener("animationend", () => { 
+    allRecords.classList.remove("all-records"); 
+    allRecords.style.display = "none";
+    realFinal.style.display = "block";
+  });
+
+  await wait(2000);
+  
+  albumInfo.innerHTML = albums[albumToDisplay].title + " - " + albums[albumToDisplay].artist + "<br><br>I discovered this one in " + albums[albumToDisplay].discovered + ".<br><br>" + ((albums[albumToDisplay].comment) ?? "");
+
+  digButton.disabled = false;
+  const records = document.querySelectorAll('.record-sleeve');
+
+  records.forEach(element => {
+    element.style.backgroundImage = "url(./record-img/" + albums[getRandomInt(albums.length)].image + ")";
+  });
 }
